0e5ae964adb593d244ef251650c4a5fab4f993cf
kuldeepdeorwinedeev <168086121+kuldeepdeorwinedeev@users.noreply.github.com>
2024-05-10 18:41:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import Fastify from "fastify";
 import authRoutes from "./routes/auth.js";
 import userManageRoute from "./routes/admin/userManage.js";
 import dabasePlugin from "./fastifyPlugin/database.js";
-import cors from "@fastify/cors";
+import { fastifyCors } from "fastify-cors"; // Import the specific function from fastify-cors
+
 const fastify = Fastify({ logger: true });
 const db = fastify.register(dabasePlugin);
 const corsOptions = {
@@ -19,24 +20,28 @@ const corsOptions = {
   optionsSuccessStatus: 200,
   exposedHeaders: "Content-Disposition",
 };
-fastify.register(require("fastify-cors"), {
-  corsOptions,
-});
+
+// Use import instead of require
+fastify.register(fastifyCors, corsOptions);
+
 authRoutes.forEach((route) => {
   const base_path = "/api/v4/user";
   route.url = base_path + route.url;
   console.log(route);
   fastify.route(route);
 });
+
 userManageRoute.forEach((route) => {
   const base_path = "/api/v4/admin/";
   route.url = base_path + route.url;
   console.log(route);
   fastify.route(route);
 });
+
 console.log({
   ...process.env,
 });
+
 const port = process.env.PORT || 4000;
 
 // Starting the server
